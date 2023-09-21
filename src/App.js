a6a61966885de70cb6d14bcf8a1cf94eeadcb64b
data-baze <60314438+data-baze@users.noreply.github.com>
2023-09-21 01:17:48 +0100

    SearchView component has been added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import AboutView from './components/AboutView';
 import {Routes, Route} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import SearchView from './components/SearchView';
 
+// 921ffd8a4b3025daed7f3998c1c654bf API 
 
 function App() {
+
+const [searchResults, setSearchResults] = useState([])
+const [searchText, setSearchText]  = useState('');
+
+
   return (
     <div>
-      <Navbar />
+      <Navbar searchText={searchText} setSearchText={setSearchText}/>
       <Routes>
-        
+
         <Route path="/" exact element={<Home />} />
         <Route path="/about" element={<AboutView />} />
+        <Route path="/search" element={<SearchView keyword={searchText} searchResults={searchResults} />} />
       
       </Routes>
     
---
+++ src/components/SearchView.js
@@ -0,0 +1,15 @@
+
+import Hero from './Hero';
+
+
+const SearchView = ({keyword, searchResults}) => {
+    const title = `You are searching for ${keyword}`
+    return (
+      <div>
+            <Hero text={title} />
+          
+      </div>
+    )
+  }
+
+  export default SearchView;
